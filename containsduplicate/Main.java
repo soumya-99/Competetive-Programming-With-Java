f97995555f7e50a137a147fec4a476dc2d2b3754
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2022-08-26 01:17:59 +0530

    contains duplicate

---
+++ containsduplicate/Main.java
@@ -0,0 +1,25 @@
+package containsduplicate;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        System.out.println(containsDuplicate(nums));
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> uniques = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            uniques.add(nums[i]);
+        }
+
+        if (uniques.size() < nums.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
