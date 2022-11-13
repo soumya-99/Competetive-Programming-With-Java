d17d8c07d1919a34309ced0f4e8d14d67eec3e60
soumya-99 <soumyadeepmondal1234@gmail.com>
2022-11-13 20:24:01 +0530

    added bubble

--- sorting_algorithms/bubble.ts
+++ sorting_algorithms/bubble.ts
@@ -1,14 +1,16 @@
 function bubble(array: number[]): number[] {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j]
-        array[j] = array[j + 1]
-        array[j + 1] = temp
-      }
+    for (let j = 1; j < array.length; j++) {
+      (array[j] < array[j - 1]) && swap(array, j, j - 1)
     }
   }
   return array
+}
+
+function swap(array: number[], i: number, j: number): void {
+  const temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
 }
 
 console.log(bubble([89, 45, 68, 90, 29, 34, 17]))
